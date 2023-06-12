d697c61793455b1a3f2cb25c8117535be6ad7400
FERNANDO GONCALVES ALBANEZ <fernando.galbanez@senacsp.edu.br>
2023-06-12 17:19:02 -0300

    segundo commit

---
+++ conexao.php
@@ -0,0 +1,28 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$password = "";
+$banco = "banco-da-popy";
+
+// Criei uma conexao
+$conexao = new mysqli($host, $usuario, $password, $banco);
+
+// Verifica se a conexão foi bem sucedida, caso nao for o banco será encerrado
+if ($conexao->connect_error) {
+  die("Connection failed: " . $conexao->connect_error);
+}
+$sql = "SELECT * FROM produto";
+$resultado = $conexao->query($sql);
+
+// Verifica se há resultados
+if ($resultado->num_rows > 0) {
+  // exibe  os dados na tela 
+  while($row = $resultado->fetch_assoc()) {
+    echo " - Nome: "       . $row["nome"] . 
+         " - Descrição: "  . $row["descricao"] . 
+  }
+} else {
+  echo "nenhum resultado"
+}
+// Fechamento da conexão 
+$conn->close();
